ea57d282f4d2dcb057c5b22daef5998ed1a9d5d2
Won Lee <jlee629@my.centennicalcollege.ca>
2024-03-29 13:00:04 -0400

    LiveData removed

--- app/src/main/java/com/example/g15_comp304sec001_lab4_ex02/MainActivity.kt
+++ app/src/main/java/com/example/g15_comp304sec001_lab4_ex02/MainActivity.kt
@@ -3,33 +3,16 @@ package com.example.g15_comp304sec001_lab4_ex02
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.text.style.TextAlign
-
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModelProvider
 import com.example.g15_comp304sec001_lab4_ex02.composable.ProgramHeader
 import com.example.g15_comp304sec001_lab4_ex02.composable.ProgramLogo
@@ -57,11 +40,6 @@ class MainActivity : ComponentActivity() {
 }
 @Composable
 fun ProgramsUI(programsViewModel: ProgramsViewModel) {
-    /*
-    val programs by programsViewModel.programs.observeAsState(initial = emptyList())
-
-    ProgramsLayer(programs = programs)
-     */
 
     val programs = remember { mutableStateListOf<Program>() }
 
